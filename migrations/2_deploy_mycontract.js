5509615af5bb142c8b1491a288a382c02ace22b3
Frozen <xrdavies@gmail.com>
2020-09-29 18:01:39 +0800

    update

--- migrations/2_deploy_mycontract.js
+++ migrations/2_deploy_mycontract.js
@@ -13,15 +13,23 @@ module.exports = function(deployer, networkName, accounts) {
 
   }).then(async ()=>{
 
-    await deployer.deploy(TokenFactory);
+    let result = await deployer.deploy(TokenFactory);
+    console.log('deploy result: ', result);
+
     let factory = await TokenFactory.deployed();
     console.log('Factory address: ', factory.address);
 
-    let tokenAddress = await factory.createERC20Token("TestToken", "TT", 18, accounts[0]);
+    console.log('accounts: ', accounts);
+    let tokenAddress = await factory.createERC20Token("TestToken", "TT", 18, tronWrap.address.toHex(accounts));
     console.log('Token address: ', tokenAddress);
 
-    let token = await ERC20Token.at(tokenAddress);
-    console.log('Token: ', token);
+    // let token = await ERC20Token.at(tokenAddress);
+    let token = await tronWrap.contract(ERC20Token.abi, tokenAddress.replace(/^41/, '0x'));
+    console.log('Token address: ', token.address);
+
+    let symbol = await token.symbol().call();
+    console.log('Created token symbol: ', symbol)
+
   }).then(async () =>  {
     
   });
