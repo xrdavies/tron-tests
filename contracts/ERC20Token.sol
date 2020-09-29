a8bbe89280c12a91a4ce2ec79e1aa14035fb0a59
Frozen <xrdavies@gmail.com>
2020-09-29 15:04:09 +0800

    update

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.8.0;
+pragma solidity >=0.4.22 <0.8.0;
 
-import "./ERC20/ERC20Detailed.sol";
-import "./ERC20/ERC20.sol";
+import "./lib/ERC20Detailed.sol";
+import "./lib/ERC20.sol";
 
 contract ERC20Token is ERC20Detailed, ERC20 {
 
@@ -13,7 +13,7 @@ contract ERC20Token is ERC20Detailed, ERC20 {
         _;
     }
 
-    constructor(string memory name, string memory symbol, uint8 decimals, address owner)
+    constructor(string memory name, string memory symbol, address owner, uint8 decimals)
         ERC20Detailed(name, symbol, decimals) public
     {
         // minter = _msgSender();
--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.8.0;
+pragma solidity >=0.4.22 <0.8.0;
 
 import "./ERC20Token.sol";
 import "./ITokenFactory.sol";
+import "./lib/Secondary.sol";
 
-contract TokenFactory is ITokenFactory {
+contract TokenFactory is ITokenFactory, Secondary {
     function createERC20Token(
         string calldata name,
         string calldata symbol,
         uint8 decimals,
         address owner
-    ) external returns (address) {
-        address token = address(new ERC20Token(name, symbol, decimals, owner));
+    ) external onlyPrimary returns (address) {
+        address token = address(new ERC20Token(name, symbol, owner, decimals));
         emit ERC20TokenCreated(token, symbol, decimals);
         return token;
     }
