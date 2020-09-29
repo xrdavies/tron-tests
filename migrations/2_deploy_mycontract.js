28d694e1b092fbbab59e8e0aa2be308745f4f565
Frozen <xrdavies@gmail.com>
2020-09-30 02:10:44 +0800

    add new test case

--- migrations/2_deploy_mycontract.js
+++ migrations/2_deploy_mycontract.js
@@ -24,27 +24,86 @@ module.exports = function (deployer, networkName, accounts) {
   }).then(async () => { // Create token directly with script
     // Using TronBox
     await deployer.deploy(ERC20Token, "FirstToken", "FTT", 18, tronWrap.address.toHex(accounts));
+
     let token = await ERC20Token.deployed();
-    console.log('[2] Token1 address: ', token.address);
+    console.log('[Directly with script] Token1 address: ', token.address);
+
     let symbol = await token.symbol();
-    console.log('[2] Created token1\'s symbol: ', symbol);
+    console.log('[Directly with script] Created token1\'s symbol: ', symbol);
 
     // Using TronWeb
     let instance = await ERC20Token.new("FirstToken2", "FTT2", 18, tronWrap.address.toHex(accounts));
     let token2 = await tronWeb.contract(instance.abi, instance.address);
-    console.log('[2] Token2 address: ', token2.address);
-    let symbol2 = await token2.symbol().call();
-    console.log('[2] Created token2\'s symbol: ', symbol2);
+    console.log('[Directly with script] Token2 address: ', token2.address);
 
-  }).then(async () => { // Create token using TokenFactory, with triggerConstantContract
+    let symbol2 = await token2.symbol().call();
+    console.log('[Directly with script] Created token2\'s symbol: ', symbol2);
+
+  }).then(async () => { // Create by script, managed by factory
+    await deployer.deploy(TokenFactory);
+
+    await deployer.deploy(ERC20Token, "Main", "MAIN", 18, myAccount);
+    let mainToken = await ERC20Token.deployed();
+    console.log('[Create by script, managed by factory] Main token address: ', mainToken.address);
+
+    let factory = await TokenFactory.deployed();
+    console.log('[Create by script, managed by factory] Factory address: ', factory.address);
+    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
+
+    let hasMain = await tokenFactory.hasSideToken(mainToken.address).send({shouldPollResponse: true});
+    console.log('[Create by script, managed by factory] has main: ', hasMain);
+    if (!hasMain) {
+      let instance = await ERC20Token.new("SideToken", "ST", 18, tokenFactory.address);
+      let sideToken = await tronWeb.contract(instance.abi, instance.address);
+      console.log('[Create by script, managed by factory] Create new side token: ', sideToken.address);
+      let receipt = await tokenFactory.addSideToken(mainToken.address, sideToken.address).send({shouldPollResponse: true});
+      console.log('[Create by script, managed by factory] Add new side token: ', receipt);
+    }
+
+    let receipt = await tokenFactory.acceptTransfer(mainToken.address, myAccount, 10000).send({shouldPollResponse: true});
+    console.log('[Create by script, managed by factory] acceptTransfer: ', receipt);
+
+  }).then(async () => { // Create token through TokenFactory and call methods through proxy
     await deployer.deploy(TokenFactory);
 
     let factory = await TokenFactory.deployed();
-    console.log('[3] Factory address: ', factory.address);
-    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
+    console.log('[Using proxy] Factory address: ', factory.address);
 
+    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
+    let tokenAddress = await tokenFactory.createERC20Token("SecondToken2", "STT2", 18, factory.address).send({ shouldPollResponse: true });
+    console.log('[Using proxy] Token address: ', tokenAddress);
+
+    const tokenSymbolFromFactory = await tokenFactory.getTokenSymbol(tokenAddress).call();
+    console.log('[Using proxy] Token symbol: ', tokenSymbolFromFactory);
+
+    await tokenFactory.mintToken(tokenAddress, myAccount, 10).send({ shouldPollResponse: true });
+    const tokenbalanceOfFromFactory = await tokenFactory.balanceOf(tokenAddress, myAccount).call();
+    console.log('[Using proxy] Token balance: ', tokenbalanceOfFromFactory.toString());
+
+  }).then(async () => { // Create token using TokenFactory
+    await deployer.deploy(TokenFactory);
+
+    let factory = await TokenFactory.deployed();
+    console.log('[Create token using TokenFactory] Factory address: ', factory.address);
+
+    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
+    let tokenAddress = await tokenFactory.createERC20Token("SecondToken2", "STT2", 18, tronWrap.address.toHex(accounts)).send({ shouldPollResponse: true });
+    console.log('[Create token using TokenFactory] Token address: ', tokenAddress);
+
+    let token = await tronWeb.contract(ERC20Token.abi, tokenAddress);
+    console.log('[Create token using TokenFactory] Token address: ', token.address);
+
+    let symbol = await token.symbol().call();
+    console.log('[Create token using TokenFactory] Created token\'s symbol: ', symbol)
+
+  }).then(async () => { // Create token using TokenFactory, with triggerConstantContract
+    await deployer.deploy(TokenFactory);
+    let factory = await TokenFactory.deployed();
+    console.log('[Using triggerConstantContract] Factory address: ', factory.address);
+
+    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
     let tokenAddress = await tokenFactory.createERC20Token("SecondToken1", "STT1", 18, tronWrap.address.toHex(accounts)).send({ shouldPollResponse: true });
-    console.log('[3] Token address: ', tokenAddress);
+    console.log('[Using triggerConstantContract] Token address: ', tokenAddress);
 
     let txn, signedTxn, receipt, txid, ret;
 
@@ -58,18 +117,17 @@ module.exports = function (deployer, networkName, accounts) {
     signedTxn = await tronWeb.trx.sign(txn.transaction, config.networks[networkName].privateKey);
     receipt = await tronWeb.trx.sendRawTransaction(signedTxn);
     txid = receipt.transaction.txID;
-    console.log('txid: ', txid);
+    console.log('[Using triggerConstantContract] txid: ', txid);
     for (let i = 0; i < 5; i++) {
       await sleep(5);
       ret = await tronWeb.trx.getTransaction(txid);
       if (ret.ret) {
-        console.log('[3] SendRawTransaction ret: ', ret);
+        console.log('[Using triggerConstantContract] SendRawTransaction ret: ', ret);
         break;
       }
-      console.log('[3] retried ', i);
+      console.log('[Using triggerConstantContract] retried ', i);
     }
 
-    /*
     txn = await tronWeb.transactionBuilder.triggerConstantContract(
       tokenAddress,
       'mint(address,uint256)',
@@ -80,42 +138,16 @@ module.exports = function (deployer, networkName, accounts) {
     signedTxn = await tronWeb.trx.sign(txn.transaction, config.networks[networkName].privateKey);
     receipt = await tronWeb.trx.sendRawTransaction(signedTxn);
     txid = receipt.transaction.txID;
-    console.log('txid: ', txid);
+    console.log('[Using triggerConstantContract] txid: ', txid);
     for (let i = 0; i < 5; i++) {
       await sleep(5);
       ret = await tronWeb.trx.getTransaction(txid);
       if (ret.ret) {
-        console.log('[3] SendRawTransaction ret: ', ret);
+        console.log('[Using triggerConstantContract] SendRawTransaction ret: ', ret);
         break;
       }
-      console.log('[3] retried ', i);
+      console.log('[Using triggerConstantContract] retried ', i);
     }
-    */
-
-  }).then(async () => { // Create token using TokenFactory
-    await deployer.deploy(TokenFactory);
-
-    let factory = await TokenFactory.deployed();
-    console.log('[4] Factory address: ', factory.address);
-
-    let tokenFactory = await tronWeb.contract(factory.abi, factory.address);
-    let tokenAddress = await tokenFactory.createERC20Token("SecondToken2", "STT2", 18, tronWrap.address.toHex(accounts)).send({ shouldPollResponse: true });
-    console.log('[4] Token address: ', tokenAddress);
-
-    // use proxy: begin
-    const tokenSymbolFromFactory = await tokenFactory.getTokenSymbol(tokenAddress).call();
-    console.log('[44] Token symbol: ', tokenSymbolFromFactory);
-
-    await tokenFactory.mintToken(tokenAddress, myAccount, 10).send({ shouldPollResponse: true });
-    const tokenbalanceOfFromFactory = await tokenFactory.balanceOf(tokenAddress, myAccount).call();
-    console.log('[44] Token balance: ', tokenbalanceOfFromFactory.toNumber());
-    // use proxy: end
-
-    let token = await tronWeb.contract(ERC20Token.abi, tokenAddress);
-    console.log('[4] Token address: ', token.address);
-
-    let symbol = await token.symbol().call();
-    console.log('[4] Created token\'s symbol: ', symbol)
 
   }).then(async () => {
     // WTF
